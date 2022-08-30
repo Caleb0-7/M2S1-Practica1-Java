05c463dc2eb7474f1218d1a4457606fc3732c2d3
CalebLC <caleblara89@gmail.com>
2022-08-30 09:46:56 -0500

    M2S1 Java Practica1

---
+++ Practica1.java
@@ -0,0 +1,70 @@
+/*
+*PRACTICA1
+*M2S2 PROGRAMACION ORIENTADA A OBJETOS
+*JOSE CALEB LARA CARRETO
+*30 DE AGOSTO DE 2022
+*/
+package practica1;
+
+/**
+ *
+ * @author caleb
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //  Byte        
+        System.out.println("MI PRIMER PROGRAMA EN JAVA");
+        System.out.println("Tipos de datos primitivos");
+        System.out.println("\nTipo de dato byte");
+        byte e1=10,e2=22,e3=44 ;
+        System.out.println("Edad 1:"+e1);
+        System.out.println("Edad 2:"+e2);
+        System.out.println("Edad 3:"+e3);
+        //Short
+        System.out.println("\nTipo de dato short");
+        short year=2022,mes=30,dia=8 ;
+        System.out.println("Year:"+year);
+        System.out.println("Mes:"+mes);
+        System.out.println("Dia:"+dia);
+        //int
+        System.out.println("\nTipo de dato int");
+        int ganancias1=45600,ganancias2=34000,ganancias3=20500;
+        System.out.println("Ganacia primer mes:"+ganancias1);
+        System.out.println("Ganancia segundo mes:"+ganancias2);
+        System.out.println("Ganancias tercer mes:"+ganancias3);
+        //long
+        System.out.println("\nTipo de dato long");
+        long empresa1=2000000,empresa2=3000000,empresa3=4000000;
+        System.out.println("Ganancia empresa 1:"+empresa1);        
+        System.out.println("Ganancia empresa 2:"+empresa2);        
+        System.out.println("Ganancia empresa 3:"+empresa3);   
+        //float
+        System.out.println("\nTipo de dato float");
+        float p1=9.7f,p2=10.9f,p3=8.7f;
+        System.out.println("Promedio1:"+p1);
+        System.out.println("Promedio2:"+p2);
+        System.out.println("Promedio3:"+p3);
+        //Double
+        //Promedio con punto decimal
+        System.out.println("\nTipo de dato Double");
+        double prom1=7.6,prom2=8.9,prom3=9.8; 
+        System.out.println("Promedio1:"+prom1);
+        System.out.println("Promedio2:"+prom2);
+        System.out.println("Promedio3:"+prom3);
+        //char
+        System.out.println("\nTipo de dato char");
+        char letra1='a',letra2='b',letra3='c';
+        System.out.println("Letra1:"+letra1);
+        System.out.println("Letra2:"+letra2);
+        System.out.println("Letra3:"+letra3);
+        //bolean
+        System.out.println("\nTipo de dato boolean");
+        boolean bandera=true;
+        System.out.println("Verdadero:"+bandera);        
+    }
+    
+}
